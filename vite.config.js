48997b784896bf21e3873e92bc1ee3b6890d8089
Muhamad Andika wardana <muhamadandikawardana@gmail.com>
2025-06-04 07:36:37 +0700

    build: add image pwa

--- vite.config.js
+++ vite.config.js
@@ -18,17 +18,17 @@ export default defineConfig({
         start_url: ".",
         icons: [
           {
-            src: "pwa-192x192.png",
+            src: "wrdnika-192.PNG",
             sizes: "192x192",
             type: "image/png",
           },
           {
-            src: "pwa-512x512.png",
+            src: "wrdnika-512.PNG",
             sizes: "512x512",
             type: "image/png",
           },
           {
-            src: "pwa-512x512.png",
+            src: "wrdnika-512.PNG",
             sizes: "512x512",
             type: "image/png",
             purpose: "any maskable",
